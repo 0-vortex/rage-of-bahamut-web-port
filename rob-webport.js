bb2136a47ae958aba4b90ff09ae48b73622b5475
vrtxf <rodoeted@yahoo.com>
2013-01-30 04:38:35 +0200

    first attempt at autoquesting

--- rob-webport.js
+++ rob-webport.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         vortex Rage of Bahamut web port
 // @namespace    http://vortex.re/
-// @version      0.3
+// @version      0.4.2
 // @description  Rage of Bahamut ios/android app web port
 // @match        http://bahamut-i.cygames.jp/bahamut_n/*
 // @copyright    2013+, Teodor Eugen Dutulescu
@@ -10,7 +10,15 @@
 var $ = unsafeWindow.jQuery;
 var Rage = {
     time: new Date().getTime(),
-    regexp: {},
+    regexp: {
+        stamina: /^STAMINA:\s([0-9]*)\/.*$/gi,
+        bossFight: /action="(.*?_quest_boss\/appear\/(?:[1-9]|1[0-9]|2[0-1])\/6)"[^>]*>\s*<input\stype="submit"\svalue="Fight\sthe\sboss!"/gi,
+        bossPlay: /"bp":"(.*?)"/gi
+    },
+    data: {
+        questThreshHold: 80,
+        stamina: 0
+    },
     uri: window.location.pathname.replace(/^\/bahamut_n\//gi, ''),
     top: $('div#top'),
     junk: ['div.toppadding', // top spacing
@@ -21,9 +29,11 @@ var Rage = {
     
     init: function () {
         Rage.myPageCheck()
-        	.removeJunk()
+			.removeJunk()
         	.fixFont()
-        	.fixCss();
+        	.fixCss()
+        	.getStamina()
+        	.quest();
         
         console.log('Loaded script in ' + (new Date().getTime() - Rage.time) / 1000 + ' seconds');
     },
@@ -79,7 +89,7 @@ var Rage = {
         WebFontConfig = {
             google: { families: [ escape(fontName) + '::latin' ] }
         };
-		var fontName = 'Roboto Condensed';
+	var fontName = 'Roboto Condensed';
         var font = document.createElement('link');
             font.type = 'text/css';
             font.href = 'http://fonts.googleapis.com/css?family=' + escape(fontName);
@@ -90,6 +100,62 @@ var Rage = {
         $('input').css('font-family', "'" + fontName + "', sans-serif !important");
         
         return Rage;
+    },
+    
+    getStamina: function () {
+        var status = $('#status', Rage.top);
+        if (status.length > 0) {
+            var staminaText = $('tr:eq(0) th:eq(1)', status);
+            
+            console.log('Main page detected, attempting to autoquest !');
+            
+            var stamina = Rage.regexp.stamina.exec(staminaText.text());
+
+            if (stamina[1] !== 'undefined') {
+                Rage.data.stamina = parseInt(stamina[1], 10);
+                
+                console.log('Current stamina: ' + Rage.data.stamina);
+            }   
+        }
+        
+        return Rage;
+    },
+    
+    quest: function () {
+        if (Rage.data.stamina >= Rage.data.questThreshHold) {
+            console.log('Executing one jump-ahead');
+            
+            $.ajax({
+                url: 'http://bahamut-i.cygames.jp/bahamut_n/app_manage/post_redirection/?url=events%2Fevent_010_quest%2Fdo_skip',
+                type: 'post',
+                success: function (data) {
+                    var bossFight = Rage.regexp.bossFight.exec(data);
+                    
+                    if (bossFight[1] !== 'undefined') {
+                        console.log('Iminent boss fight detected, attempting to kill boss:');
+
+                        $.ajax({
+                            url: 'http://bahamut-i.cygames.jp/bahamut_n/app_manage/post_redirection/?url=events%2Fevent_010_quest_boss%2Fboss_play_swf%2F21',
+                            type: 'post',
+                            data: {
+                                accesskey_5: 'Fight Boss'
+                            },
+                            success: function (data) {
+                                var bp = Rage.regexp.bossPlay.exec(data);
+                                
+                                console.log(bp);
+                                
+                                console.log('Boss fight successfully completed !');
+                            }
+                        });
+                    }
+                    
+                    else {
+        		console.log('Sylvan jump ahead executed successfully !');
+                    }
+                }
+            });
+        }
     }
 };
 
